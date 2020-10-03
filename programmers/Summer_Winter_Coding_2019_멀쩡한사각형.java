b15d49c80946a61218b4d86579934147aa4f83d4
kimjw9302 <jaewoong9302@gmail.com>
2020-10-03 16:36:03 +0900

    2020.10.03 - 프로그래머스 서머 윈터 코딩 멀쩡한 사각형 - 걸린 시간 : 1시간

---
+++ programmers/Summer_Winter_Coding_2019_멀쩡한사각형.java
@@ -0,0 +1,19 @@
+/*
+*	url  httpsprogrammers.co.krlearncourses30lessons62048#
+*/
+import java.lang.Math;
+
+class Solution {
+    public long solution(int w, int h) {
+        long answer = 0;
+        double tan = (double)hw;
+         0 1 2 3 4 5 ,,, w-1
+        for(int x = 1 ; x  w ; x++){
+            double temp = xtan;
+            int ceilVal = (int)Math.ceil(temp);
+            answer += h - ceilVal;
+        }     
+        answer=answer2;
+        return answer;
+    }
+}
