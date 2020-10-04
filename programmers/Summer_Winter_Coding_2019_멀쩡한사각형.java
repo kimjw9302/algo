85c19dfd63200fe418a0afa55ddd8adc0a2b08ee
kimjw9302 <jaewoong9302@gmail.com>
2020-10-04 14:36:07 +0900

    멀쩡한사각형 오타 수정

--- programmers/Summer_Winter_Coding_2019_멀쩡한사각형.java
+++ programmers/Summer_Winter_Coding_2019_멀쩡한사각형.java
@@ -1,19 +1,19 @@
 /*
-*	url  httpsprogrammers.co.krlearncourses30lessons62048#
+*	url :  https://programmers.co.kr/learn/courses/30/lessons/62048
 */
 import java.lang.Math;
 
 class Solution {
     public long solution(int w, int h) {
         long answer = 0;
-        double tan = (double)hw;
-         0 1 2 3 4 5 ,,, w-1
-        for(int x = 1 ; x  w ; x++){
-            double temp = xtan;
+        double tan = (double)h/w;
+        // 0 1 2 3 4 5 ,,, w-1
+        for(int x = 1 ; x < w ; x++){
+            double temp = x*tan;
             int ceilVal = (int)Math.ceil(temp);
             answer += h - ceilVal;
         }     
-        answer=answer2;
+        answer=answer*2;
         return answer;
     }
 }
